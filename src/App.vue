fb122d6a2f12c63202eed86a86f7cb5335037263
eduardobautistag <eduardo.bautista@delfosti.com>
2022-08-18 11:24:25 -0500

    f

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<pokedex/>
+</template>
+
+<script>
+import pokedex from "./components/pokedex.vue";
+
+export default {
+  name: "App",
+  components: {
+    pokedex
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
